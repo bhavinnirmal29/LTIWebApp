486a06c894c8d9e66584101d9281bc3f0aa330bb
Bhavin <bhavinnirmal29@gmail.com>
2021-04-01 16:27:36 +0530

    Add project files.

---
+++ LTIWebApp/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LTIWebApp.Models;
+namespace LTIWebApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        LTIMVCEntities db1 = new LTIMVCEntities();
+        [HttpGet]
+        // GET: Login
+        public ActionResult LoginPage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult LoginPage(Registration reg)
+        {
+            
+            string uname = Request.Form["username"];
+            string pwd = Request.Form["pwd"];
+            var data = db1.Registrations.Where(x => x.Username.Equals(uname) && x.Pwd == pwd).SingleOrDefault();
+            if(data==null)
+            {
+                ModelState.AddModelError("", "Login Failed");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Login Successfull");
+            }
+            return View();
+        }
+    }
+}
---
+++ LTIWebApp/Controllers/RegisterController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LTIWebApp.Models;
+namespace LTIWebApp.Controllers
+{
+    public class RegisterController : Controller
+    {
+        LTIMVCEntities db = new LTIMVCEntities();
+        // GET: Register
+        [HttpGet]
+        public ActionResult RegisterPage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult RegisterPage(Registration reg)
+        {
+            string uname = Request.Form["username"];
+            string pwd = Request.Form["pwd"];
+            string email = Request.Form["email1"];
+            string city = Request.Form["city"];
+            string mob = Request.Form["mobile"];
+            reg.Username = uname;
+            reg.Pwd = pwd;
+            reg.Email = email;
+            reg.City = city;
+            reg.Mobile = mob;
+            db.Registrations.Add(reg);
+            int res = db.SaveChanges();
+            if (res > 0)
+                ModelState.AddModelError("", "One Row Inserted");
+            else
+                ModelState.AddModelError("", "No Rows Inserted");
+
+            return View();
+        }
+    }
+}
